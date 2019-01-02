199e916387d2a24751b57fc3edd9736eddb86894
Xi Yang <yunxiache@gmail.com>
2019-01-01 22:56:14 -0500

    start to refactor initial code using new infrastructure on OOP

--- rxnorm_restful_api.py
+++ rxnorm_restful_api.py
@@ -1,9 +1,57 @@
-# NDC code related
-NDC_PROPERTIES = "https://rxnav.nlm.nih.gov/REST/ndcproperties?id={}"
-NDC_STATUS = "https://rxnav.nlm.nih.gov/REST/ndcstatus?ndc={}"
+import traceback
+import sys
 
-# rxcui related
-RXCUI2INGREDIENT = "https://rxnav.nlm.nih.gov/REST/rxcui/{}/filter?propName=TTY&propValues=IN"
-RXCUI_ALLRELATED = "https://rxnav.nlm.nih.gov/REST/rxcui/{}/allrelated"
-RXCUI_ALLPROPERTIES = "https://rxnav.nlm.nih.gov/REST/rxcui/{}/allProperties.json?prop=attributes"
-RXCUI_HISTORY = "https://rxnav.nlm.nih.gov/REST/rxcuihistory/concept?rxcui={}"
+
+class RXNORM_RESTFUL_API:
+    def __init__(self):
+        # base URL
+        self.__ROOT = "https://rxnav.nlm.nih.gov/REST/"
+        # NDC code related
+        self.NDC_PROPERTIES = "ndcproperties?id={}"
+        self.NDC_STATUS = "ndcstatus?ndc={}"
+        # rxcui related
+        self.RXCUI_ALLRELATED = "rxcui/{}/allrelated"
+        self.RXCUI_ALLPROPERTIES = "rxcui/{}/allProperties.json?prop=attributes"
+        self.RXCUI_HISTORY = "rxcuihistory/concept?rxcui={}"
+        # rxcui and ingredients relation
+        self.IS_RXCUI_IN = "rxcui/{}/filter?propName=TTY&propValues=IN"
+        # relation between ndc and rxnorm
+        self.RXCUI2NDC = "/rxcui/{}/allhistoricalndcs"
+
+    def get_ndc_properties_by_id(self, ndc_id):
+        return self.__ROOT + self.NDC_PROPERTIES.format(ndc_id)
+
+    def get_ndc_status_by_ndc_code(self, ndc_code):
+        return self.__ROOT + self.NDC_STATUS.format(ndc_code)
+
+    def get_rxcui_allrelated(self, rxcui):
+        return self.__ROOT + self.RXCUI_ALLRELATED.format(rxcui)
+
+    def get_rxcui_history(self, rxcui):
+        return self.__ROOT + self.RXCUI_HISTORY.format(rxcui)
+
+    def get_rxcui_allproperties(self, rxcui):
+        return self.__ROOT + self.RXCUI_ALLPROPERTIES.format(rxcui)
+
+    def get_rxcui2ndc(self, rxcui):
+        return self.__ROOT + self.RXCUI2NDC.format(rxcui)
+
+    def get_is_rxcui_ingredient(self, rxcui):
+        return  self.__ROOT + self.IS_RXCUI_IN.format(rxcui)
+
+
+def test():
+    rxnorm_api = RXNORM_RESTFUL_API()
+
+    try:
+        print(rxnorm_api.__ROOT + rxnorm_api.NDC_PROPERTIES.format(2018))
+    except:
+        print("It will raise an exception with trace: ")
+        traceback.print_exc(file=sys.stdout)
+        print("\n")
+
+    print(rxnorm_api.get_ndc_properties_by_id(2018))
+
+
+if __name__ == '__main__':
+    test()
--- rxnormq.py
+++ rxnormq.py
@@ -1,6 +1,67 @@
 import requests
-import logging
+import traceback
+from rxnorm_restful_api import RXNORM_RESTFUL_API
+from config import create_logger, REQUESTS_TIMEOUT
+
+
+# header used for requests to get json returned from get
+headers = {'Accept': 'application/json',
+           'user-agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
 
 
 class RxNorm:
-    pass
+    def __init__(self):
+        self.rxnorm_restful_api = RXNORM_RESTFUL_API()
+        self.logger = None
+        self.timeout = REQUESTS_TIMEOUT
+        self.session = requests.Session()
+
+    def task(self, task='is_rxcui_ingredient', **kwargs):
+        self.logger = create_logger(task)
+        self.logger.info(f'start task {task}...')
+        if 'timeout' in kwargs:
+            self.timeout = kwargs.get('timeout')
+
+        if task == 'is_rxcui_ingredient':
+            assert 'rxcui' in kwargs, 'rxcui code must be provided for this task'
+            return self.__task_is_rxcui_ingredient(rxcui=kwargs.get('rxcui'))
+        elif task == 'rxcui2ndc':
+            pass
+        elif task == 'ndc2rxcui':
+            assert 'ndc' in kwargs, 'ndc code must be provided for this task'
+            return self.__task_ndc2rxcui(ndc=kwargs.get('ndc'))
+
+        # reset timeout to config timeout
+        self.timeout = REQUESTS_TIMEOUT
+
+    def __task_is_rxcui_ingredient(self, rxcui):
+        rest_api = self.rxnorm_restful_api.get_is_rxcui_ingredient(rxcui)
+        self.logger.info(f'restful api: {rest_api}')
+        try:
+            info = self.session.get(rest_api, headers=headers, timeout=self.timeout)
+            self.logger.info(f'status: {info.status_code}')
+            if info.text == 'null':
+                return False
+            return True
+        except requests.exceptions.ReadTimeout:
+            self.logger.info(f'request time out for checking rxcui code: {rxcui}')
+        return False
+
+    def __task_ndc2rxcui(self, ndc):
+        pass
+
+
+def test():
+    rxnorm = RxNorm()
+    print(rxnorm.task(rxcui=316326))
+    print(rxnorm.task(rxcui=7052))
+    print(rxnorm.task(rxcui=105918, timeout=1))
+    try:
+        print(rxnorm.task())
+    except:
+        traceback.print_exc()
+
+
+
+if __name__ == '__main__':
+    test()
